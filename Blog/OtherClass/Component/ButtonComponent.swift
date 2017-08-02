4139beace157d61e26df5148e6e4fb72b1f7513b
LuoHengC <853542000@qq.com>
2017-08-02 21:44:53 +0800

    增加button类

---
+++ Blog/OtherClass/Component/ButtonComponent.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonComponent.swift
+//  Blog
+//
+//  Created by Vayne on 2017/8/2.
+//  Copyright © 2017年 luoheng. All rights reserved.
+//
+
+import UIKit
+
+class ButtonComponent: UIButton {
+    
+}
